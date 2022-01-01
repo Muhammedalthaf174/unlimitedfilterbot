d3abeed181f679afa95e7fed4a2899531a7fcfc5
Muhammedalthaf174 <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-01-01 21:06:28 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,14 +47,14 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Movie Group", url="https://t.me/Goodwillmovies"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/milnabotchannel"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/milnabotsgroup")
                 ]
-            ]
+            ]   
         )
 
         await query.message.edit_text(
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/milnabotchannel")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),

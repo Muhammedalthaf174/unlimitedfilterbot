9734df6545061921acf0c854d9d0a5cbd35d9034
Muhammedalthaf174 <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-01-01 20:55:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,8 +2,8 @@ class Script(object):
 
     START_MSG = """<b>Hy {},
 
-I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
+I'm 𝑇𝑢𝑐𝑎❥︎.. Iam A filter bot with many capabilities!
+Add Me in Your Groups And see My Power..
 
 See <i>/help</i> for commands and more details.</b>
 """
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @Althafser</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : 𝑇𝑢𝑐𝑎❥︎</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @Althafser   
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
